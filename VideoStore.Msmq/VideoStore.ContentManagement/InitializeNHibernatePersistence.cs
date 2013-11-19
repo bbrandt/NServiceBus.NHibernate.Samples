5b775fc51b1cbcbbd84fe5cdf35575530dddafda
indualagarsamy <indu.alagarsamy@gmail.com>
2013-11-18 17:56:37 -0800

    Added the video store (msmq) sample which works with NHibernate persistence.

---
+++ VideoStore.Msmq/VideoStore.ContentManagement/InitializeNHibernatePersistence.cs
@@ -0,0 +1,15 @@
+﻿namespace VideoStore.ContentManagement
+{
+    using NServiceBus;
+
+    class InititalizeSubscriptionStorage : IWantCustomInitialization
+    {
+        public void Init()
+        {
+            NServiceBus.Configure.Instance
+                .UseNHibernateSubscriptionPersister() // subscription storage using NHibernate
+                .UseNHibernateTimeoutPersister() // Timeout Persistance using NHibernate
+                .UseNHibernateSagaPersister(); // Saga Persistance using NHibernate
+        }
+    }
+}
